cd979e26668ab8407efa683e4a745f1c4012340e
Bryan's Air <BryansAppleAir@BryBook.local>
2018-05-18 13:37:51 -0600

    style: add line spacing between line 22 and line 24

--- FatImages-Step1/FatImages/ViewController.swift
+++ FatImages-Step1/FatImages/ViewController.swift
@@ -20,6 +20,7 @@ import UIKit
 // https connection is, fear not! Everything will be explained in the
 // networking section.
 // At this point it's not relevant.
+
 enum BigImages: String {
     case whale = "https://lh3.googleusercontent.com/16zRJrj3ae3G4kCDO9CeTHj_dyhCvQsUDU0VF0nZqHPGueg9A9ykdXTc6ds0TkgoE1eaNW-SLKlVrwDDZPE=s0#w=4800&h=3567"
     case shark = "https://lh3.googleusercontent.com/BCoVLCGTcWErtKbD9Nx7vNKlQ0R3RDsBpOa8iA70mGW2XcC76jKS09pDX_Rad6rjyXQCxngEYi3Sy3uJgd99=s0#w=4713&h=3846"
